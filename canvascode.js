07f53c46e4fb89354e205c0cfbe3aa8bc17824f4
Srujan Deshpande <srujan.deshpande@gmail.com>
2019-11-25 12:19:58 +0530

    COlors

--- canvascode.js
+++ canvascode.js
@@ -1,5 +1,27 @@
 var c = document.getElementById("gameCanvas");
 var ctx = c.getContext("2d");
 
-ctx.fillStyle = "red";
+ctx.fillStyle = "white";
 ctx.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
+ctx.strokestyle = "black";
+ctx.strokeRect(0, 0, gameCanvas.width, gameCanvas.height);
+
+ctx.fillStyle = 'lightgreen';
+ctx.strokestyle = 'darkgreen';
+ctx.fillRect(0, 0, 10, 10);
+ctx.strokeRect(0, 0, 10, 10);
+ctx.fillRect(100, 100, 10, 10);
+ctx.strokeRect(100, 100, 10, 10);
+//ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
+//ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
+
+function drawSnakePart(snakex,snakey) {
+  ctx.fillStyle = 'lightgreen';
+  ctx.strokestyle = 'darkgreen';
+  ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
+  ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
+}
+
+function drawSnake() {
+
+}
