944c8f1627efb6bce17598e18389df504f83c489
Srujan Deshpande <srujan.deshpande@gmail.com>
2019-11-26 17:47:18 +0530

    Made board gen a function

--- canvascode.js
+++ canvascode.js
@@ -1,27 +1,17 @@
-var c = document.getElementById("gameCanvas");
-var ctx = c.getContext("2d");
+genBoard()
+function genBoard() {
+  var c = document.getElementById("gameCanvas");
+  var ctx = c.getContext("2d");
 
-ctx.fillStyle = "white";
-ctx.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
-ctx.strokestyle = "black";
-ctx.strokeRect(0, 0, gameCanvas.width, gameCanvas.height);
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
+  ctx.strokestyle = "black";
+  ctx.strokeRect(0, 0, gameCanvas.width, gameCanvas.height);
 
-ctx.fillStyle = 'lightgreen';
-ctx.strokestyle = 'darkgreen';
-ctx.fillRect(0, 0, 10, 10);
-ctx.strokeRect(0, 0, 10, 10);
-ctx.fillRect(gameCanvas.width-10, gameCanvas.height10-10, 10, 10);
-ctx.strokeRect(gameCanvas.width-10, gameCanvas.height10-10, 10, 10);
-//ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
-//ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
-
-function drawSnakePart(snakex,snakey) {
   ctx.fillStyle = 'lightgreen';
   ctx.strokestyle = 'darkgreen';
-  ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
-  ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
-}
-
-function drawSnake() {
-
+  ctx.fillRect(0, 0, 10, 10);
+  ctx.strokeRect(0, 0, 10, 10);
+  ctx.fillRect(gameCanvas.width-10, gameCanvas.height-10, 10, 10);
+  ctx.strokeRect(gameCanvas.width-10, gameCanvas.height-10, 10, 10);
 }
