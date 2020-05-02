8981cfa7bbf876d309150536712ef911a58344db
Srujan Deshpande <srujan.deshpande@gmail.com>
2020-05-02 23:58:27 +0530

    more junk

--- highscores.php
+++
@@ -1,8 +0,0 @@
-<?php
-  $highscores = fopen("hscores.txt","r") or $highscores = [0,0,0,0,0];
-  $hs = strtok($highscores,",");
-  echo '<script>';
-  echo 'var one = document.getElementById(\'one\');';
-  echo 'one.innerHTML = '.json_encode($hs).';';
-  echo '</script>';
- ?>
